c46cd6460e8560edd821bcdf84f9d72e35ab9d9b
Hayden-M-Kearney <142760478+Hayden-M-Kearney@users.noreply.github.com>
2023-08-22 12:15:26 -0400

    Alphabetize! v1.0.py
    Organizational tool for large media libraries such as music, videos, books, video games, etc.
    
    Has two functions:
    1. Alphabetize! Will take a target folder's contents and move them into sub-folders based on the first letter of each filename. Files beginning with any number (0-9) will be placed into a folder named "#".
    
    2. Undo. Will undo the Alphabetize! function by moving all the contents within the sub-folders of the target folder back into the target folder. Empty folders named "#" or "A"-"Z" will be deleted upon completion.

---
+++ Alphabetize! v1.0.py
@@ -0,0 +1,94 @@
+import os
+import shutil
+import tkinter as tk
+from tkinter import ttk, messagebox, filedialog
+
+class AlphabetizeApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Alphabetize!")
+        self.destination_folder = tk.StringVar()
+
+        ttk.Label(root, text="Target Folder:").pack(pady=10)
+        self.target_frame = ttk.Frame(root)
+        self.target_frame.pack(pady=5)
+        
+        target_entry = ttk.Entry(self.target_frame, textvariable=self.destination_folder)
+        target_entry.pack(side=tk.LEFT)
+        
+        ttk.Button(self.target_frame, text="Select Folder", command=self.select_folder).pack(side=tk.LEFT)
+        
+        button_frame = ttk.Frame(root)
+        button_frame.pack(pady=10)
+        ttk.Button(button_frame, text="Alphabetize!", command=self.alphabetize_files).pack(side=tk.LEFT, padx=5)
+        ttk.Button(button_frame, text="Undo", command=self.undo_files).pack(side=tk.LEFT, padx=5)
+        
+        notebook = ttk.Notebook(root)
+        notebook.pack(fill='both', expand=True)
+        
+        home_tab = ttk.Frame(notebook)
+        notebook.add(home_tab, text='Home')
+        
+        help_text = tk.Text(home_tab, wrap=tk.WORD, padx=10, pady=10)
+        help_text.insert(tk.END, "Welcome to Alphabetize!\n\n", "center bold")
+        help_text.insert(tk.END, "TO USE:\n", "center")
+        help_text.insert(tk.END, "Choose a target folder with the 'Select Folder' button and press 'Alphabetize!'\n\nThe Alphabetize! function will move the target folder's contents into Alphabetized! sub-folders with a '#' folder for contents starting with numbers.\n\n", "center")
+        help_text.insert(tk.END, "NOTES:\n", "center")
+        help_text.insert(tk.END, "1. Folders within the target folder will be moved without altering their contents.\n\n2. The 'Undo' function will only delete empty sub-folders named '#' or 'A'-'Z' upon completion.")
+        help_text.tag_configure("center", justify="center")
+        help_text.tag_configure("bold", font=("Helvetica", 12, "bold"))
+        help_text.config(state=tk.DISABLED)
+        help_text.pack(fill='both', expand=True)
+
+    def select_folder(self):
+        self.destination_folder.set(filedialog.askdirectory())
+    
+    def alphabetize_files(self):
+        dest_folder = self.destination_folder.get()
+        if not dest_folder:
+            messagebox.showerror("Error", "Please select a target directory.")
+            return
+        
+        for filename in os.listdir(dest_folder):
+            if filename != os.path.basename(__file__):
+                first_char = filename[0]
+                if first_char.isdigit():
+                    subfolder_path = os.path.join(dest_folder, "#")
+                else:
+                    first_char = first_char.upper()
+                    subfolder_path = os.path.join(dest_folder, first_char)
+                    
+                os.makedirs(subfolder_path, exist_ok=True)
+                shutil.move(os.path.join(dest_folder, filename), os.path.join(subfolder_path, filename))
+        
+        messagebox.showinfo("Operation Complete", "The target folder's contents have been Alphabetized!")
+
+    def undo_files(self):
+        dest_folder = self.destination_folder.get()
+        if not dest_folder:
+            messagebox.showerror("Error", "Please select a target folder.")
+            return
+        
+        for root, dirs, files in os.walk(dest_folder, topdown=False):
+            for dir_name in dirs[:]:
+                if dir_name == "#" or (len(dir_name) == 1 and dir_name.isalpha()):
+                    dir_path = os.path.join(root, dir_name)
+                    for filename in os.listdir(dir_path):
+                        shutil.move(os.path.join(dir_path, filename), os.path.join(dest_folder, filename))
+                    os.rmdir(dir_path)
+                
+                    dirs.remove(dir_name)
+        
+            for filename in files:
+                shutil.move(os.path.join(root, filename), os.path.join(dest_folder, filename))
+        
+        messagebox.showinfo("Operation Complete", "Alphabetize! undone. Empty sub-folders have been deleted.")
+
+def main():
+    root = tk.Tk()
+    app = AlphabetizeApp(root)
+    root.geometry("500x430")
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
